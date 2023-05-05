91325b2cbcafc15732ab38c24669c71a1994e131
Simon-Olivier Lachance <49047004+monstrotac@users.noreply.github.com>
2023-05-05 12:49:36 -0400

    Create GlobalMarketDataObject.cs

---
+++ SkinHound/SkinHound/GlobalMarketDataObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkinHound
+{
+    public class GlobalMarketDataObject
+    {
+        public SteamHistory Steam { get; set; }
+        public Buff163History Buff163 { get; set; }
+    }
+    public class SteamHistory
+    {
+        public double Last_24h { get; set; }
+        public double Last_7d { get; set; }
+        public double Last_30d { get; set; }
+        public double Last_90d { get; set; }
+    }
+    public class Buff163History
+    {
+        public double Starting_At { get; set; }
+        public double Highest_Order { get; set; }
+    }
+}
